2a2bfb5cc7ccc9daf20057011fa5f0f0603b18af
napontah <napon02@hotmail.com>
2023-08-07 23:17:54 +0000

    Update ML code

--- ML/Train-test-split_05082023.r
+++ ML/Train-test-split_05082023.r
@@ -1,5 +1,31 @@
 library(tidyverse)
 library(caret)
 
-##test push again
-##test pull again
+## Classification And Regression Tree => caret
+## Created by Max Kuhn
+
+# Simple ML pipeline
+# 0. prep data/clean data
+# 1. split data => train test split
+# 2. train model
+# 3. score model aka. prediction
+# 4. evaluate model
+
+full_df <- mtcars %>%
+  select(mpg, hp, wt, am)
+
+# check missing value
+full_df %>%
+  complete.cases() %>%
+  mean()
+
+# clean data - drop row with NA
+clean_df <- full_df %>%
+  drop_na()
+
+
+
+
+
+
+
