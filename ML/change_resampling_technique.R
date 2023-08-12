5a5f1b844afaa12d708810fd8ce7e7e74c66e59e
napontah <napon02@hotmail.com>
2023-08-12 11:23:49 +0000

    Update code 12/8/2023

--- ML/change_resampling_technique.R
+++ ML/change_resampling_technique.R
@@ -29,3 +29,45 @@ model <- train(medv ~ rm + b + crim,
                method = "lm",
                trControl = ctrl)
 
+## use K-Fold CV technique
+## Golden rule
+set.seed(24)
+ctrl <- trainControl(
+  method = "cv", # default CV is K-Fold
+  number = 5, 
+  verboseIter = TRUE # to print resampling data log
+)
+
+
+
+model <- train(medv ~ rm + b + crim,
+               data = train_data,
+               method = "lm",
+               preProcess = c("center", "scale"), # for standardization (z-score)
+               # preProcess = c("range"), for normalization
+               trControl = ctrl)
+
+## variable importance
+varImp(model)
+
+
+## repeated K-Fold CV technique
+
+set.seed(24)
+ctrl <- trainControl(
+  method = "repeatedcv", # repeated K-Fold
+  number = 5, # k = 5
+  repeats = 5, # repeats = 5
+  verboseIter = TRUE # to print resampling data log
+)
+
+
+
+model <- train(medv ~ rm + b + crim,
+               data = train_data,
+               method = "lm",
+               trControl = ctrl)
+
+## variable importance
+varImp(model)
+
